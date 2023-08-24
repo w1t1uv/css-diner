2509e293cd3f3e357f4082dc011dc9d5570172d3
Egor Ushakov <witluv@icloud.com>
2023-08-24 22:01:37 +0300

    feat: add the level loading function to the page

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,18 @@
 import {initLevel} from './init/level';
+import {firstLevelCreator} from './creator/first';
+
+let level = 1;
+let isGameOver = false;
+let levelWin = false;
+
+function loadLevel() {
+    const storageLevel = localStorage.getItem('storageLevel');
+
+    if (storageLevel === '1') {
+        firstLevelCreator();
+    }
+}
 
 initLevel();
+loadLevel();
+
